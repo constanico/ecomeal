45b62aca3d022586eb9b8e37119012d9ae607ef0
JasonStation <jasonleonardo88@gmail.com>
2023-09-04 19:45:06 +0700

    Initial Commit

---
+++ ECOMEAL/ECOMEALApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECOMEALApp.swift
+//  ECOMEAL
+//
+//  Created by Jason Leonardo on 04/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ECOMEALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
